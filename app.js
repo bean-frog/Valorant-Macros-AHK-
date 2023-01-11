bce99bb6961833f5adc34de280b19246334962ef
bean-frog <104943861+bean-frog@users.noreply.github.com>
2023-01-11 11:24:30 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,24 @@
 let strings = {
     string1: instaSova,
     string2: instaJett,
-    string3: "Option 3 selected\n"
+    string3: instaSage,
+	string4: instaSkye,
+	string5: instaHarbor,
+	string6: instaChamber,
+	string7: instaRaze,
+	string8: instaPhoenix,
+	string9: instaOmen,
+	string10: instaReyna,
+	string11: instaNeon,
+	string12: instaFade,
+	string13: instaBreach,
+	string14: instaBrimstone,
+	string15: instaKilljoy,
+	string16: instaViper,
+	string17: instaYoru,
+	string18: instaKayo,
+	string19: instaCypher,
+	string20: instaFade,
 };
 $('#append-button').on('click', function() {
     $('#output').empty(); // clear the contents of the "output" div
